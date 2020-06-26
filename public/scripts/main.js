b884a427b20ff0f7ba34b9b05f132a245132b8be
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-06-26 16:41:47 -0400

    Fix landing

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
 function resetHeight(){
     // reset the body height to that of the inner browser
-    document.body.style.height = window.innerHeight + "px";
+    const landing = document.getElementById('landing').style.height = window.innerHeight + "px";
 }
 // reset the height whenever the window's resized
 window.addEventListener("resize", resetHeight);
