611f2964aaecca01f70675fc6c887bde7390259c
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-06-26 16:50:49 -0400

    Fix responsive

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,12 +10,3 @@ $(document).ready(function () {
     });
 });
 
-
-function resetHeight(){
-    // reset the body height to that of the inner browser
-    const landing = document.getElementById('landing').style.height = window.innerHeight + "px";
-}
-// reset the height whenever the window's resized
-window.addEventListener("resize", resetHeight);
-// called to initially set the height.
-resetHeight();
