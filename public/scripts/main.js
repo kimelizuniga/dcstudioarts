1d7d5a074931bb12f486eb3e631d329bc1380193
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-06-28 14:27:55 -0400

    Fix landing height between section

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var scroll_pos = 0;
     $("#landing").scroll(function () {
         scroll_pos = $(this).scrollTop();
-        if (scroll_pos > 500) {
+        if (scroll_pos > 300) {
             $("#landingContent").css('background-color', 'rgba(0,0,0,0.6');
         } else {
             $("#landingContent").css('background-color', 'rgba(255,255,255,0.1)');
