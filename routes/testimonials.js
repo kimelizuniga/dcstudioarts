c0226be24c009d28f46bb3b2787c21e5db552c16
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-06-27 21:27:17 -0400

    Finish testimonials functionality

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -38,4 +38,56 @@ router.post('/', middleware.isLoggedIn, (req, res) => {
     })
 })
 
+// TESTIMONIALS EDIT PAGE
+
+router.get('/edit', (req, res) => {
+    Testimony.find({}, (err, allTestimonials)=>{
+        if(err){
+            console.log(err)
+        } else {
+            res.render('testimonials/edits', {testimonials: allTestimonials})
+        }
+    })
+})
+
+router.get('/edit/:id/edit', (req, res)=>{
+    Testimony.findById(req.params.id, (err, foundTestimony) => {
+        res.render('testimonials/edit', {testimony: foundTestimony})
+    })
+})
+
+router.put('/edit/:id', (req, res) => {
+    Testimony.findByIdAndUpdate(req.params.id, req.body.testimony, (err, testimony) => {
+        if(err){
+            req.flash('error', err.message);
+            res.redirect('back');
+        } else {
+            req.flash('success','Successfully Updated!');
+            res.redirect('/testimonials/edit');
+        }
+    });
+})
+
+// DELETE TESTIMONY
+
+
+router.get('/edit/:id/delete', (req, res) => {
+    Testimony.findById(req.params.id, (err, foundTestimony) => {
+        res.render('testimonials/delete', {testimony: foundTestimony})
+    })
+})
+
+router.delete('/edit/:id', (req, res) => {
+    Testimony.findById(req.params.id, (err, gallery) => {
+        if(err){
+            res.redirect('/testimonies/edit')
+        } else {
+            gallery.remove();
+            req.flash('success', 'Testimony removed successfully')
+            res.redirect('/testimonials/edit')
+        }
+    })
+})
+
+
 module.exports = router;
